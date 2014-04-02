cb39fbcf07b04d41647893cb1093ecb93118a46c
Pamela Prosperi <pamepros@gmail.com>
2014-04-02 17:30:23 -0300

    add spinning preloaders and methods in app class

--- www/js/index.js
+++ www/js/index.js
@@ -98,6 +98,12 @@ var app = {
 			);
 		}catch(e) { }
 	},
+    startSpinning: function(){
+        $("#btn-config").show();
+    },
+    stopSpinning: function(){
+        $("#btn-config").hide();
+    },
 
 };
 
